235510551cc46b821625a9273bbd1e1a1c4316f9
Savik Fraguela <34078966+savikdf@users.noreply.github.com>
2023-11-03 23:49:53 -1000

    Style rework to gulp powered sass framework

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+// var uglify = require('gulp-uglify');
+// var concat = require('gulp-concat');
+
+gulp.task('sass', function(done){
+
+    gulp.src('src/sass/**/*.scss')
+      .pipe(sass({outputStyle: 'compressed'})) // Using gulp-sass
+      .pipe(gulp.dest('src/dist'))
+
+      done();
+  });
+
+// gulp.task('scripts', function(done){
+
+//     gulp.src('public/javascripts/**/*.js')
+//         .pipe(concat('scripts.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('public/javascripts'))
+
+//         done();
+// });
+
+
+gulp.task('watch', function(done){
+    gulp.watch('src/sass/**/*.scss', gulp.series('sass') )
+    // gulp.watch('public/javascripts/**/*.js', gulp.series('scripts') )
+    done();
+})
