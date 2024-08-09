cc9eb4bc6e51909609ea94e767a3cd267922ff53
Savik Fraguela <34078966+savikdf@users.noreply.github.com>
2024-08-08 23:42:22 -1000

    big JS refactor, and started working on hero feature

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,16 @@ window.onload = () => {
   console.log("Welcome to SavikLand.com, Enjoy your stay!");
 };
 
-var sectionModule = (function () {
-  let config = {
+SL_sectionModule = {
+  config: {
     sectionInViewClass: "visible",
     sectionScrollStyleKey: "--scroll-val",
     observer_even: null,
     observer_odd: null,
     visibleSections: [],
-  };
-  //init
-  function initialize() {
+  },
+
+  init: function() {
     return;
     let sections = [...document.querySelectorAll("main > section")];
     if (sections == null && sections.length == 0) return;
@@ -23,9 +23,8 @@ var sectionModule = (function () {
 
     initializeIntersectionObservers(sections);
     initializeScrollObserver(sections);
-  }
-
-  function initializeIntersectionObservers(sections) {
+  },
+  initializeIntersectionObservers: function(sections) {
 
     const observer_odd = generateObserver();
     const observer_even = generateObserver();
@@ -40,34 +39,31 @@ var sectionModule = (function () {
 
     config.observer_odd = observer_odd;
     config.observer_even = observer_even;
-  }
-
-  function initializeScrollObserver(sections) {
+  },
+  initializeScrollObserver: function(sections) {
     window.addEventListener("scroll", onScroll);
     sections.forEach(section =>{
       section.style.setProperty("--total-height", section.offsetHeight);
     })
-  }
+  },
 
   //handlers/callbacks
-  function onEnter(section) {
+  onEnter: function(section) {
     //console.log("just entered: ", section);
     if (!section.classList.contains(config.sectionInViewClass)) {
       section.classList.add(config.sectionInViewClass);
       section.dataset.visible = "";
       config.visibleSections.push(section);
     }
-  }
-
-  function onExit(section) {
+  },
+  onExit: function(section) {
     //console.log("just exited: ", section);
     section.classList.remove(config.sectionInViewClass);
     delete section.dataset.visible;
     section.style.removeProperty(config.sectionScrollStyleKey);
     removeElementFromArray(config.visibleSections, section);
-  }
-
-  function generateObserver(){
+  },
+  generateObserver: function(){
     return new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -79,9 +75,8 @@ var sectionModule = (function () {
         }
       });
     });
-  }
-
-  function onScroll() {
+  },
+  onScroll: function() {
     return;
     const sections = config.visibleSections; 
     sections.forEach((section) => {
@@ -105,30 +100,115 @@ var sectionModule = (function () {
       // Set the scroll percent as a CSS variable for the sections styling purposes
       section.style.setProperty(config.sectionScrollStyleKey, roundedStyleValue);
     });
-  }
+  },
 
   //helpers
-  function observeElement(element, onEnterCallback, onExitCallback) {
+  observeElement: function(element, onEnterCallback, onExitCallback) {
     
 
     // Return the observer in case you need to disconnect it later
     return observer;
-  }
-
-  function removeElementFromArray(array, elementToRemove) {
+  },
+  removeElementFromArray: function(array, elementToRemove) {
     const index = array.indexOf(elementToRemove);
     if (index !== -1) {
       array.splice(index, 1);
     }
-  }
+  },
+};
 
-  return {
-    init: initialize,
-  };
-})().init();
+SL_eventsModule = {
+  config: {
+    debugKeyCode : 190,
+    debugEnabled : false,
+    mouseXPercent: 0.01,
+    mouseYPercent: 0.01,
+    scrollbarLenience: -15,
+    isMouseOnPage: false,
+    timer_noMouseMovement: 0.00,
+    timer_noMouseMovementStart: null,
+    timer_noMouseMovementDelta: null,
+  },  
 
-var siteDataModule = (function(){
-  let config = {
+  init: function(){
+    const _ = this;
+    //internal
+    document.addEventListener("mousemove", SL_eventsModule.handleMouseMove);
+    document.addEventListener("mouseenter", SL_eventsModule.handleMouseEnter);
+    document.addEventListener("mouseleave", SL_eventsModule.handleMouseLeave);
+    document.addEventListener("keydown", SL_eventsModule.handleKeyDown);
+    //external
+    document.addEventListener("mousemove", SL_siteDataModule.eyeTrack);
+
+    setInterval(_.doTimer, 1000); // update about every second
+  },
+
+  //handlers
+  handleMouseMove: function(event) {
+    const _ = SL_eventsModule;
+    _.config.timer_noMouseMovement = 0;
+    _.config.timer_noMouseMovementStart = Date.now();
+
+    var eventDoc, doc, body;
+
+    event = event || window.event; // IE-ism
+
+    // If pageX/Y aren't available and clientX/Y are,
+    // calculate pageX/Y - logic taken from jQuery.
+    // (This is to support old IE)
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+          (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+          (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+
+    //saving percent vars into config
+    _.config.mouseXPercent = Math.round(((event.pageX / (window.innerWidth + _.config.scrollbarLenience)) + Number.EPSILON) * 100) / 100;
+    _.config.mouseYPercent = Math.round(((event.pageY / window.innerHeight) + Number.EPSILON) * 100) / 100;
+
+    //debug ui
+    document.getElementById('debug-mousePercent').textContent = `{X:${_.config.mouseXPercent}, Y:${_.config.mouseYPercent}}`;
+  },
+  handleKeyDown: function(event){
+    const _ = SL_eventsModule;
+    if(event.keyCode == _.config.debugKeyCode){
+      _.config.debugEnabled = !_.config.debugEnabled;
+      document.getElementById('debug-menu').dataset.debugEnabled = _.config.debugEnabled;
+    }
+
+  },
+  handleMouseEnter: function(event){
+    const _ = SL_eventsModule;
+    _.config.isMouseOnPage = true;
+    document.getElementById("debug-mouseOn").textContent = _.config.isMouseOnPage;
+  },
+  handleMouseLeave: function(event){
+    console.log("out")
+    const _ = SL_eventsModule;
+    _.config.isMouseOnPage = false;
+    document.getElementById("debug-mouseOn").textContent = _.config.isMouseOnPage;
+  },
+
+  doTimer: function(){
+    const _ = SL_eventsModule;
+    console.log("doing timer")
+    _.config.timer_noMouseMovementDelta = Date.now() - _.config.timer_noMouseMovementStart; // milliseconds elapsed since start
+    _.config.timer_noMouseMovement = Math.floor(_.config.timer_noMouseMovementDelta / 1000); // in seconds
+    document.getElementById('debug-mouseInactiveTimer').textContent = _.config.timer_noMouseMovement;
+  },
+
+};
+
+SL_siteDataModule = {
+  config : {
+    cardSectionId: "job-cards",
     projectCardTemplate: `
       <div class="card">
         <h2 class="card__header"><span>{num}</span>{company}</h2>
@@ -136,39 +216,57 @@ var siteDataModule = (function(){
         {responsibilities}
       </div>
       `,
-  };
-  function initialize() {
-    refreshProjectCards();
-  }
+  },
+  init: function() {
+    const _ = this;
+    _.refreshProjectCards();
+  },
 
-  function refreshProjectCards(){
+  refreshProjectCards: function(){
+    const _ = this;
     fetch('../../scripts/savik.json')
         .then(response => response.json())
         .then(resumeData => {
           let cards = [];
 
           resumeData.jobs.forEach((job, index) => {
-            cards.push(generateProjectCard(job, index + 1));            
+            cards.push(_.generateProjectCard(job, index + 1));            
           });
 
-          let insertDiv = document.getElementById('job-cards');
-          console.log('about to pipe it in', {insertDiv, cards});
+          let insertDiv = document.getElementById(_.config.cardSectionId);
           insertDiv.innerHTML = cards.join('');
         })
         .catch(error => {
           console.error('Error refreshing Project Cards', error);
         });
-  }
+  },
+  eyeTrack: function(){
+    let eyes = [...document.querySelectorAll('.eye')];
+    if(document.hidden || SL_eventsModule.config.timer_noMouseMovement > 2){
+      //return to default state, no longer watching mouse
+      // eye.forEach(eye => {
+      //   eye.style.setProperty("--hor-offset",  )
+      // });
+
+      return;
+    }
+
+    eyes.forEach(eye => {
+      let offset = (SL_eventsModule.config.mouseXPercent * 100) - 50; //minus 50 to have the baseline as the middle of the screen
+      eye.style.setProperty("--hor-offset", `${offset}px`);
+    });
+  },
 
   // generators 
-  function generateProjectCard(job, index){
-    return config.projectCardTemplate
+  generateProjectCard: function(job, index){
+    const _ = this;
+    return _.config.projectCardTemplate
       .replace("{num}", index)
       .replace("{company}", job.company)
       .replace("{position}", job.position)
-      .replace("{responsibilities}", generateProjectItemList(job.responsibilities));
-  };
-  function generateProjectItemList(items){
+      .replace("{responsibilities}", _.generateProjectItemList(job.responsibilities));
+  },
+  generateProjectItemList: function(items){
     let result = '<ul>'
 
     items.forEach((item, i) => {
@@ -176,9 +274,32 @@ var siteDataModule = (function(){
     });
 
     return result + '</ul>'
-  };
+  },
+}
 
-  return {
-    init: initialize,
-  };
-})().init();
+MainModule = {
+  config:{
+    modules: [],
+  },
+  init: function(){
+    const _ = this;
+    _.config.modules = _.initializeNamedModules("SL_");
+  },
+  initializeNamedModules: function(prefix){
+    const _ = this;
+    let moduleNames = _.getPrefixedModuleNames(prefix);
+    let moduleObjects = [];
+    moduleNames.forEach(name => {
+      const module = window[name];
+      if (module && typeof module.init === 'function') {
+        moduleObjects.push();
+        module.init();
+      }
+    });
+    return moduleObjects;
+  },
+  getPrefixedModuleNames : function (prefix){
+    return Object.keys(window).filter(key => key.startsWith(prefix));
+  },
+};
+MainModule.init();
